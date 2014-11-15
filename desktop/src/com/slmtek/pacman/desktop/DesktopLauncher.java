92538d162b9c7ea523960b883ad801ff65a01f76
Ke Ma <kema2012@gmail.com>
2014-11-15 01:32:41 +0000

    initial build libgdx project

---
+++ desktop/src/com/slmtek/pacman/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.slmtek.pacman.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.slmtek.pacman.Pacman;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Pacman(), config);
+	}
+}
